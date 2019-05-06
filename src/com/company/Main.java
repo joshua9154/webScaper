351c2f0da02c886a272a4e68971b3b1add5202bc
joshua <joshuasteinerman@gmail.com>
2019-05-06 16:05:27 -0400

    Eighth Version(1000 emails in one minute)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,11 +11,13 @@ class SharedObject {
     public static List<String> emailList = Collections.synchronizedList(new ArrayList<String>());
     public static Collection<String> visitedSites = Collections.synchronizedCollection(new HashSet<>());
 
+
 }
 
 class spiders extends Thread {
     static Object lock = new Object();
     static int MAX_COUNT = 100;
+    static int marker=20;
     private ArrayList<String> sitesFound;
     private ArrayList<String> emailsFound;
 
@@ -33,7 +35,8 @@ class spiders extends Thread {
                 WebSpider crawler = new WebSpider();
                Random rand = new Random();
                 int r = rand.nextInt(SharedObject.siteList.size() - 1);
-                if (!SharedObject.visitedSites.contains(SharedObject.siteList.get(r))) {
+                //Most websites that have emails have a character length less than 70
+                if (!SharedObject.visitedSites.contains(SharedObject.siteList.get(r))&&SharedObject.siteList.get(r).length()<70) {
 
                     SharedObject.visitedSites.add(SharedObject.siteList.get(r));
                     sitesFound = crawler.getLinks(SharedObject.siteList.get(r));
@@ -45,6 +48,7 @@ class spiders extends Thread {
                     }
                     for (String k : emailsFound) {
                         if (!SharedObject.emailList.contains(k)) {
+                           // SharedObject.emailList.add("Site Length "+SharedObject.siteList.get(r).length());
                             SharedObject.emailList.add(k);
                         }
                     }
@@ -56,11 +60,17 @@ class spiders extends Thread {
       //  System.out.println("ID " + currentThread().getId());
         // System.out.println("Sites Found "+SharedObject.siteList.size());
         // System.out.println("Sites Visted "+SharedObject.visitedSites.size());
-        System.out.println("Emails Found " + SharedObject.emailList.size());
+     //   System.out.println("Websites Found "+SharedObject.siteList.size());
+      //  System.out.println("Emails Found " + SharedObject.emailList.size());
        // System.out.println(SharedObject.emailList.get(SharedObject.emailList.size() - 1));
     //    System.out.println(SharedObject.emailList.toString());
         // System.out.println(Thread.currentThread().getId());
-
+         if(SharedObject.emailList.size()>=marker){
+             System.out.println("Sites Visted "+SharedObject.visitedSites.size());
+             System.out.println("Emails Found " + SharedObject.emailList.size());
+             System.out.println(SharedObject.emailList.toString());
+             marker=marker+50;
+         }
     }
 }
 
